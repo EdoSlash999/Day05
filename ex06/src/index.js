dca08616fafb621545ac2dedffed55e48fd83b58
EdoSlash999 <edo_tz_@hotmail.com>
2021-03-13 10:21:57 +0100

    zavrseni zadaci

---
+++ ex06/src/index.js
@@ -0,0 +1,11 @@
+function main(){
+    // Only change code below this line
+    var myName = "Edin";
+    var myString = "My name is " + myName + " and I am Well!"
+    // only change code above this line
+
+    return myString;
+}
+
+console.log(main());
+module.exports = main;
---
+++ ex10/src/index.js
@@ -0,0 +1,11 @@
+function main(){
+    // Only change code below this line
+    var myString = "Jellow World";
+    myString = "Hello World";
+    // only change code above this line
+
+    return myString;
+}
+
+console.log(main());
+module.exports = main;
